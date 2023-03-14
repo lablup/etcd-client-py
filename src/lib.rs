b9b38832c395c6619feca8daf8716c5ff8fe2d50
Seo Sanghyeon <seosh@lablup.com>
2023-03-14 20:09:15 +0900

    Implement watch

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,18 @@ use std::sync::Arc;
 
 use etcd_client::Client as RustClient;
 use etcd_client::Error as RustError;
-use etcd_client::{DeleteOptions, GetOptions};
+use etcd_client::Event as RustEvent;
+use etcd_client::EventType as RustEventType;
+use etcd_client::WatchStream as RustStream;
+use etcd_client::{DeleteOptions, GetOptions, WatchOptions};
 use pyo3::create_exception;
-use pyo3::exceptions::PyException;
+use pyo3::exceptions::{PyException, PyStopAsyncIteration};
 use pyo3::prelude::*;
+use pyo3::pyclass::CompareOp;
 use pyo3::types::PyTuple;
 use pyo3_asyncio::tokio::future_into_py;
 use tokio::sync::Mutex;
+use tokio_stream::StreamExt;
 
 create_exception!(etcd_client, ClientError, PyException);
 
@@ -18,6 +23,56 @@ struct Error(RustError);
 impl From<Error> for PyErr {
     fn from(error: Error) -> Self {
         ClientError::new_err(error.0.to_string())
+    }
+}
+
+#[pyclass]
+#[derive(PartialEq, Eq, Clone)]
+struct EventType(RustEventType);
+
+#[pymethods]
+impl EventType {
+    #[classattr]
+    const PUT: Self = Self(RustEventType::Put);
+
+    #[classattr]
+    const DELETE: Self = Self(RustEventType::Delete);
+}
+
+#[pyclass]
+#[derive(PartialEq, Eq, Clone)]
+struct Event {
+    key: String,
+    value: String,
+    prev_value: Option<String>,
+    event: EventType,
+}
+
+#[pymethods]
+impl Event {
+    #[new]
+    #[pyo3(signature = (key, value, prev_value, event))]
+    fn new(key: String, value: String, prev_value: Option<String>, event: EventType) -> Self {
+        Self { key, value, prev_value, event }
+    }
+
+    fn __richcmp__(&self, py: Python<'_>, other: &Self, op: CompareOp) -> PyObject {
+        match op {
+            CompareOp::Eq => (self == other).into_py(py),
+            CompareOp::Ne => (self != other).into_py(py),
+            _ => py.NotImplemented()
+        }
+    }
+}
+
+impl From<RustEvent> for Event {
+    fn from(event: RustEvent) -> Self {
+        let kv = event.kv().unwrap();
+        let key = String::from_utf8(kv.key().to_owned()).unwrap();
+        let value = String::from_utf8(kv.value().to_owned()).unwrap();
+        let prev_value = None;
+        let event = EventType(event.event_type());
+        Self { key, value, prev_value, event }
     }
 }
 
@@ -155,11 +210,125 @@ impl Communicator {
                 .map_err(|e| Error(e).into())
         })
     }
+
+    fn watch(&self, key: String) -> Watch {
+        let client = self.0.clone();
+        Watch::new(client, key, None)
+    }
+
+    fn watch_prefix(&self, key: String) -> Watch {
+        let client = self.0.clone();
+        let options = WatchOptions::new().with_prefix();
+        Watch::new(client, key, Some(options))
+    }
+}
+
+struct Stream {
+    stream: RustStream,
+    events: Vec<Event>,
+    index: usize,
+}
+
+impl Stream {
+    fn new(stream: RustStream) -> Self {
+        Self {
+            stream,
+            events: Vec::new(),
+            index: 0,
+        }
+    }
+
+    async fn next(&mut self) -> Option<Result<Event, Error>> {
+        if self.index < self.events.len() {
+            let event = self.events[self.index].clone();
+            self.index += 1;
+            return Some(Ok(event));
+        }
+        let option = self.stream.next().await;
+        let result = match option {
+            Some(result) => result,
+            None => return None
+        };
+        let response = match result {
+            Ok(response) => response,
+            Err(error) => return Some(Err(Error(error)))
+        };
+        let mut events = Vec::new();
+        for event in response.events() {
+            events.push(event.clone().into());
+        }
+        self.events = events;
+        let event = self.events[0].clone();
+        self.index = 1;
+        Some(Ok(event))
+    }
+}
+
+#[pyclass]
+#[derive(Clone)]
+struct Watch {
+    client: Arc<Mutex<RustClient>>,
+    key: String,
+    options: Option<WatchOptions>,
+    stream: Option<Arc<Mutex<Stream>>>,
+}
+
+impl Watch {
+    fn new(client: Arc<Mutex<RustClient>>, key: String, options: Option<WatchOptions>) -> Self {
+        Self {
+            client,
+            key,
+            options,
+            stream: None,
+        }
+    }
+
+    async fn init(&mut self) -> Result<(), Error> {
+        if !self.stream.is_none() {
+            return Ok(());
+        }
+        let mut client = self.client.lock().await;
+        let key = self.key.clone();
+        let options = self.options.clone();
+        let result = client.watch(key, options).await;
+        result
+            .map(|(_, stream)| {
+                self.stream = Some(Arc::new(Mutex::new(Stream::new(stream))));
+                ()
+            })
+            .map_err(|e| Error(e))
+    }
+}
+
+#[pymethods]
+impl Watch {
+    fn __aiter__(&self) -> Self {
+        self.clone()
+    }
+
+    fn __anext__<'a>(&'a self, py: Python<'a>) -> Option<PyObject> {
+        let watch = Arc::new(Mutex::new(self.clone()));
+        let result = future_into_py(py, async move {
+            let mut watch = watch.lock().await;
+            watch.init().await?;
+            let stream = watch.stream.as_ref().unwrap().clone();
+            let mut stream = stream.lock().await;
+            let option = stream.next().await;
+            let result = match option {
+                Some(result) => result,
+                None => return Err(PyStopAsyncIteration::new_err(()))
+            };
+            Ok(result?)
+        });
+        Some(result.unwrap().into())
+    }
 }
 
 #[pymodule]
 #[pyo3(name = "etcd_client")]
 fn init(_py: Python<'_>, module: &PyModule) -> PyResult<()> {
     module.add_class::<Client>()?;
+    module.add_class::<Event>()?;
+    module.add_class::<EventType>()?;
     Ok(())
 }
